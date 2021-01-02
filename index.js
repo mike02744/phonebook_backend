fd65331f8a30d543574f111d24b613b6d6a14f97
Mingyuan Hua <huamy2019@gmail.com>
2021-01-01 22:25:48 -0500

    add frontend build

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("build"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :id")
 );
