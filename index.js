7e39ed1c473a78a2d0aa543b5d1fb0578df2d9d6
Mingyuan Hua <huamy2019@gmail.com>
2021-01-03 04:25:03 -0500

    finish exercise 3.21

--- index.js
+++ index.js
@@ -24,22 +24,28 @@ app.post("/api/persons", (req, res, next) => {
       error: "name or number misssing",
     });
   }
-  const temp = persons.find((person) => person.name === body.name);
-  if (temp) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
-  }
+  // const temp = persons.find((person) => person.name === body.name);
+  // if (temp) {
+  //   return res.status(400).json({
+  //     error: "name must be unique",
+  //   });
+  // }
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((savedperson) => {
-    res.json(savedperson.toJSON());
-  });
+  person
+    .save()
+    .then((savedperson) => savedperson.toJSON())
+    .then((formatedperson) => res.json(formatedperson))
+    .catch((error) => next(error));
+
+  // person.save().then((savedperson) => {
+  //   res.json(savedperson.toJSON());
+  // });
   // console.log("person", person);
-  persons = persons.concat(person);
+  // persons = persons.concat(person);
 });
 
 app.delete("/api/persons/:id", (req, res, next) => {
@@ -69,18 +75,10 @@ app.put("/api/persons/:id", (req, res, next) => {
     number: body.number,
   };
   Person.findByIdAndUpdate(req.params.id, person, { new: true })
-    .then((updatedPerson) => {
-      res.json(updatedPerson.toJSON());
-    })
+    .then((updatedPerson) => updatedPerson.toJSON())
+    .then((formatedperson) => res.json(formatedperson))
     .catch((error) => next(error));
 });
-
-// app.get("/info", (req, res) => {
-//   const num = persons.length;
-//   const info = `<p>Phonebook has info for ${num} people</p><p>${Date()}</p>`;
-
-//   res.send(info).end();
-// });
 
 app.get("/api/persons", (req, res) => {
   // res.json(persons);
@@ -98,8 +96,15 @@ app.use(unknownEndpoint);
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
-  if (error.name === "CastError" && error.kind === "ObjectId") {
+  // if (error.name === "CastError" && error.kind === "ObjectId") {
+  //   return response.status(400).send({ error: "malformatted id" });
+  // }
+  if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
