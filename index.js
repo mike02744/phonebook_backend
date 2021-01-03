2edce2c13ffa931dbc7c4ffafab5e9506ee45a8b
Mingyuan Hua <huamy2019@gmail.com>
2021-01-03 02:34:29 -0500

    finish 3.18

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
-const { response } = require("express");
-// const { response } = require("express");
+
 morgan.token("id", (req) => {
   return JSON.stringify(req.body);
 });
@@ -17,36 +16,6 @@ app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :id")
 );
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
-const generateId = (max, min) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  const id = Math.floor(Math.random() * (max - min + 1) + min);
-  return id;
-};
 
 app.post("/api/persons", (req, res, next) => {
   const body = req.body;
@@ -64,14 +33,13 @@ app.post("/api/persons", (req, res, next) => {
   const person = new Person({
     name: body.name,
     number: body.number,
-    // id: generateId(100, 1000),
   });
 
   person.save().then((savedperson) => {
     res.json(savedperson.toJSON());
   });
+  // console.log("person", person);
   persons = persons.concat(person);
-  res.json(person);
 });
 
 app.delete("/api/persons/:id", (req, res, next) => {
@@ -80,12 +48,6 @@ app.delete("/api/persons/:id", (req, res, next) => {
       res.status(204).end();
     })
     .catch((error) => next(error));
-
-  // const id = Number(req.params.id);
-  // persons = persons.filter((person) => person.id !== id);
-
-  // console.log(persons);
-  // res.status(204).end();
 });
 
 app.get("/api/persons/:id", (req, res, next) => {
@@ -98,14 +60,6 @@ app.get("/api/persons/:id", (req, res, next) => {
       }
     })
     .catch((error) => next(error));
-  // const id = Number(req.params.id);
-  // const person = persons.find((person) => person.id === id);
-
-  // if (person) {
-  //   res.json(person);
-  // } else {
-  //   res.status(404).end();
-  // }
 });
 
 app.put("/api/persons/:id", (req, res, next) => {
@@ -114,22 +68,19 @@ app.put("/api/persons/:id", (req, res, next) => {
     name: body.name,
     number: body.number,
   };
-  console.log(person);
   Person.findByIdAndUpdate(req.params.id, person, { new: true })
     .then((updatedPerson) => {
-      console.log(updatedPerson);
-      // res;
       res.json(updatedPerson.toJSON());
     })
     .catch((error) => next(error));
 });
 
-app.get("/info", (req, res) => {
-  const num = persons.length;
-  const info = `<p>Phonebook has info for ${num} people</p><p>${Date()}</p>`;
+// app.get("/info", (req, res) => {
+//   const num = persons.length;
+//   const info = `<p>Phonebook has info for ${num} people</p><p>${Date()}</p>`;
 
-  res.send(info).end();
-});
+//   res.send(info).end();
+// });
 
 app.get("/api/persons", (req, res) => {
   // res.json(persons);
